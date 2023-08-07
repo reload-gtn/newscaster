22dcd83cb2f940e3bc2e3c68562f33554911fd6c
Aleksandr <reload-gtn@mail.ru>
2023-08-07 17:20:16 +0300

    first

--- init.lua
+++ init.lua
@@ -1,3 +1,12 @@
 newscaster = {}
 
 print("Start mod newscaster at load time server!")
+minetest.chat_send_all("This is a chat message to all players")
+minetest.register_chatcommand("mtsr", {
+    privs = {
+        interact = true,
+    },
+    func = function(name, param)
+        return true, "MTSR: " .. param .. "!"
+    end,
+})
