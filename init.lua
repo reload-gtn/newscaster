b5f20f73de6904e0c1e3b9e608e72cc09d0e2849
Aleksandr <reload-gtn@mail.ru>
2023-08-07 17:00:45 +0300

    begin

--- init.lua
+++ init.lua
@@ -1,15 +1,3 @@
 newscaster = {}
 
 print("Start mod newscaster at load time server!")
-
-minetest.register_node("newscaster:node", {
-    description = "This is a node",
-    tiles = { "newscaster_node.png" },
-    groups = { cracky = 1 }
-})
-
-minetest.register_craft({
-    type = "shapeless",
-    output = "newscaster:node 3",
-    recipe = { "default:dirt", "default:stone" },
-})
