78533f91fcde11e0be25bd1488a7a4c2996ebb23
Aleksandr <aleksandr@cultLinuxPC>
2023-08-07 20:04:54 +0000

    add news bot

--- init.lua
+++ init.lua
@@ -1,7 +1,29 @@
 newscaster = {}
 
-print("Start mod newscaster at load time server!")
-minetest.chat_send_all("This is a chat message to all players")
+local i = 1
+local duration = 2
+local t = {}
+--local colour = minetest.rgba(255, 0, 0, 0)
+local nameserver =  minetest.colorize("red", "MTSR: ")
+--local nameserver = "MTSR: "
+
+t[1] = "Я - новостной канал"
+t[2] = "Вы - игроки сервера"
+t[3] = "Наша задача - играть"
+t[4] = "Стройте вместе, так быстрее!"
+
+
+local function printtext()
+    minetest.chat_send_all(nameserver .. t[i])
+    if i == #t then
+        i = 1
+    else
+        i = i + 1
+    end
+    minetest.after(duration, printtext)
+end
+minetest.after(duration, printtext)
+
 minetest.register_chatcommand("mtsr", {
     privs = {
         interact = true,
